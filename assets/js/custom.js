d0dda418b91e236dec7959591f0221e247d19804
Brenna Nieva <bnieva101@gmail.com>
2019-07-24 14:40:00 -0700

    file rearrange

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,22 +1,5 @@
-/**	
-	* Template Name: Kevin
-	* Version: 1.0	
-	* Template Scripts
-	* Author: MarkUps
-	* Author URI: http://www.markups.io/
+/
 
-	Custom JS
-	
-	1. FIXED MENU
-	2. FEATURED SLIDE ( TYPED SLIDER )
-	3. SKILL PROGRESS BAR
-	4. MENU SMOOTH SCROLLING
-	5. MOBILE MENU CLOSE  
-	6. PORTFOLIO GALLERY
-	7. PORTFOLIO POPUP VIEW ( IMAGE LIGHTBOX )
-	8. CLIENT TESTIMONIALS ( SLICK SLIDER )
-	9. BUTTON SMOOTH SCROLL ( VIEW MY WORK )
-	
 **/
 
 
@@ -24,26 +7,16 @@
 (function( $ ){
 
 
-	/* ----------------------------------------------------------- */
-	/*  2. FIXED MENU
-	/* ----------------------------------------------------------- */
-
-
 	jQuery(window).bind('scroll', function () {
 
     if ($(window).scrollTop() > 100) {
 
         $('#mu-header').addClass('mu-fixed-nav');
-        
+
 	    } else {
 	        $('#mu-header').removeClass('mu-fixed-nav');
 	    }
 	});
-
-		
-	/* ----------------------------------------------------------- */
-	/*  2. FEATURED SLIDE (TYPED SLIDER)
-	/* ----------------------------------------------------------- */
 
 		var typed = new Typed('#typed', {
 		    stringsElement: '#typed-strings',
@@ -54,10 +27,6 @@
 		    loopCount: Infinity
 		});
 
-		
-	/* ----------------------------------------------------------- */
-	/*  3. SKILL PROGRESS BAR
-	/* ----------------------------------------------------------- */
 
 		$('.mu-skill-progress-bar').appear(function() {
 
@@ -92,106 +61,64 @@
 
 	/* ----------------------------------------------------------- */
 	/*  4. MENU SMOOTH SCROLLING
-	/* ----------------------------------------------------------- */ 
+	/* ----------------------------------------------------------- */
 
-		//MENU SCROLLING WITH ACTIVE ITEM SELECTED
 
-		// Cache selectors
 		var lastId,
 		topMenu = $(".mu-menu"),
 		topMenuHeight = topMenu.outerHeight()+13,
-		// All list items
+
 		menuItems = topMenu.find('a[href^=\\#]'),
-		// Anchors corresponding to menu items
 		scrollItems = menuItems.map(function(){
 		  var item = $($(this).attr("href"));
 		  if (item.length) { return item; }
 		});
 
-		// Bind click handler to menu items
-		// so we can get a fancy scroll animation
 		menuItems.click(function(e){
 		  var href = $(this).attr("href"),
 		      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+22;
-		  jQuery('html, body').stop().animate({ 
+		  jQuery('html, body').stop().animate({
 		      scrollTop: offsetTop
 		  }, 1500);
 		  e.preventDefault();
 		});
 
-		// Bind to scroll
 		jQuery(window).scroll(function(){
-		   // Get container scroll position
 		   var fromTop = $(this).scrollTop()+topMenuHeight;
-		   
-		   // Get id of current scroll item
+
 		   var cur = scrollItems.map(function(){
 		     if ($(this).offset().top < fromTop)
 		       return this;
 		   });
-		   // Get the id of the current element
 		   cur = cur[cur.length-1];
 		   var id = cur && cur.length ? cur[0].id : "";
-		   
+
 		   if (lastId !== id) {
 		       lastId = id;
-		       // Set/remove active class
+
 		       menuItems
 		         .parent().removeClass("active")
 		         .end().filter("[href=\\#"+id+"]").parent().addClass("active");
-		   }           
+		   }
 		});
 
 
 	/* ----------------------------------------------------------- */
-	/*  5. MOBILE MENU CLOSE 
-	/* ----------------------------------------------------------- */ 
+	/*  5. MOBILE MENU CLOSE
+	/* ----------------------------------------------------------- */
 
 		jQuery('.mu-menu').on('click', 'li a', function() {
 		  $('.in').collapse('hide');
 		});
 
 
-	/* ----------------------------------------------------------- */
-	/*  6. PORTFOLIO GALLERY
-	/* ----------------------------------------------------------- */ 
-		$('.filtr-container').filterizr();
 
-		//Simple filter controls
 
-	    $('.mu-simplefilter li').click(function() {
-	        $('.mu-simplefilter li').removeClass('active');
-	        $(this).addClass('active');
-	    });
 
-	/* ----------------------------------------------------------- */
-	/*  7. PORTFOLIO POPUP VIEW ( IMAGE LIGHTBOX )
-	/* ----------------------------------------------------------- */ 
-
-	$('.mu-filter-imglink').magnificPopup({
-	  type: 'image',
-	  mainClass: 'mfp-fade',
-	  gallery:{
-	    enabled:true
-	  }
-	});
-
-	/* ----------------------------------------------------------- */
-	/*  8. CLIENT TESTIMONIALS (SLICK SLIDER)
-	/* ----------------------------------------------------------- */
-
-		$('.mu-testimonial-slide').slick({
-		  arrows: false,
-		  dots: true,
-		  infinite: true,
-		  speed: 500,
-		  autoplay: true,
-		  cssEase: 'linear'
-		});
 
 
 	/* ----------------------------------------------------------- */
-	/*  9. BUTTON SMOOTH SCROLL ( VIEW MY WORK )
+	/*  9. BUTTON SMOOTH SCROLL 
 	/* ----------------------------------------------------------- */
 
 		$('.view-my-work-btn').on('click',function (e) {
@@ -207,10 +134,5 @@
 
 
 
-	
+
 })( jQuery );
-
-
-
-  
-	
