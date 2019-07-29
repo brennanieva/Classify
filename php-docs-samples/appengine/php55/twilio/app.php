d7c65e73440af46b4dbe214747716d9ca85cdbdd
cssi <cssi@D-10-19-184-217.dhcp4.washington.edu>
2019-07-28 22:31:26 -0700

    dead

--- php-docs-samples/appengine/php55/twilio/app.php
+++ php-docs-samples/appengine/php55/twilio/app.php
@@ -1,11 +1,30 @@
 <?php
-$client = new Services_Twilio($sid, $token);
-$sms = $client->account->messages->sendMessage(
-    $fromNumber, // From this number
-    $toNumber,   // Send to this number
-    'Hello monkey!!'
-);
 
-return sprintf('Message ID: %s, Message Body: %s', $sms->sid, $sms->body);
-
-?>
+use Silex\Application;
+$app = new Application();
+$app->get('/', function () use ($app) {
+    if ($app['twilio.account_sid'] == 'TWILIO_ACCOUNT_SID') {
+        return 'set your Twilio SID and Auth Token in <code>index.php</code>';
+    }
+    $sid = $app['twilio.account_sid'];
+    $token = $app['twilio.auth_token'];
+    $fromNumber = $app['twilio.from_number'];
+    $toNumber = $app['twilio.to_number'];
+    # [START send_sms]
+    $client = new Services_Twilio($sid, $token);
+    $sms = $client->account->messages->sendMessage(
+        $fromNumber, // From this number
+        $toNumber,   // Send to this number
+        'kill me now plz i want to stab my eye out with a fork idc if i die thank you and goodnight'
+    );
+    return sprintf('Message ID: %s, Message Body: %s', $sms->sid, $sms->body);
+    # [END send_sms]
+});
+$app->post('/twiml', function () {
+    # [START twiml]
+    $response = new Services_Twilio_Twiml();
+    $response->say('Hello Monkey');
+    return (string) $response;
+    # [END twiml]
+});
+return $app;
